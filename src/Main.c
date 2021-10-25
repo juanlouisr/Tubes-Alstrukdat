79ea010a0613e077b4f03227eee9671fd4e634d1
mizuday <mizuday5.7@gmail.com>
2021-10-25 21:30:36 +0700

    test input out stdin dan file config

--- src/Main.c
+++ src/Main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include "adtlib.h"
+#include "utillib.h"
 
 int main()
 {
-    printf("test");
+    printLogo();
+    printMenu();
+    FILE* file = fopen("src/data/config.cfg", "r");
+    if (file != NULL)
+    {
+        int row = getIntInputStream(file);
+        int col = getIntInputStream(file);
+        printf("row : %d, col : %d\n", row, col);
+    }
+    
+    printf(">> ");
+    Word word = getWordSTDIN();
+
+    if (isWordEQ(word, "1"))
+    {
+        printf("NEW GAME!\n");
+        
+    }
+    else if (isWordEQ(word, "2"))
+    {
+        printf("LOAD GAME!\n");
+    }
+    else
+    {
+        printf("SAMPAI JUMPA LAGI!\n");
+    }
+    
+
+    // printf("hasil get word stdin: ");
+    // wordOutputStream(stdout, word, true);
+
+    // char c = getCharSTDIN();
+    // printf("hasil get char: %c\n", c);
+
+    
+
     return 0;
 }
---
+++ src/utillib.h
@@ -0,0 +1,7 @@
+#ifndef __UTIL_LIB_H__
+#define __UTIL_LIB_H__
+
+#include "utility/header/iolib.h"
+#include "utility/header/pcolor.h"
+
+#endif
