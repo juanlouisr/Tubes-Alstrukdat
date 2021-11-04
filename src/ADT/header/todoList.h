ebcecd4ca1e3a0b76846621b2c48e7323b239a5e
vincen-tho <16520229@std.stei.itb.ac.id>
2021-11-04 16:51:29 +0700

    Added todoList

---
+++ src/ADT/header/todoList.h
@@ -0,0 +1,91 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include "boolean.h"
+#include "../originaladt/node.h"
+
+typedef Address List;
+
+// #define IDX_UNDEF (-1)
+#define FIRST(l) (l)
+
+/* Definisi List : */
+/* List kosong : FIRST(l) = NULL */
+/* Setiap elemen dengan Address p dapat diacu INFO(p), NEXT(p) */
+/* Elemen terakhir list: jika addressnya Last, maka NEXT(Last)=NULL */
+
+/* PROTOTYPE */
+/****************** PEMBUATAN LIST KOSONG ******************/
+void CreateListTodoList(List *l);
+/* I.S. sembarang             */
+/* F.S. Terbentuk list kosong */
+
+/****************** TEST LIST KOSONG ******************/
+boolean isEmptyTodoList(List l);
+/* Mengirim true jika list kosong */
+
+/****************** GETTER SETTER ******************/
+ElType getElmtTodoList(List l, int idx);
+/* I.S. l terdefinisi, idx indeks yang valid dalam l, yaitu 0..length(l) */
+/* F.S. Mengembalikan nilai elemen l pada indeks idx */
+
+void setElmtTodoList(List *l, int idx, ElType val);
+/* I.S. l terdefinisi, idx indeks yang valid dalam l, yaitu 0..length(l) */
+/* F.S. Mengubah elemen l pada indeks ke-idx menjadi val */
+
+int indexOfTodoList(List l, ElType val);
+/* I.S. l, val terdefinisi */
+/* F.S. Mencari apakah ada elemen list l yang bernilai val */
+/* Jika ada, mengembalikan indeks elemen pertama l yang bernilai val */
+/* Mengembalikan IDX_UNDEF jika tidak ditemukan */
+
+/****************** PRIMITIF BERDASARKAN NILAI ******************/
+/*** PENAMBAHAN ELEMEN ***/
+void insertFirstTodoList(List *l, ElType val);
+/* I.S. l mungkin kosong */
+/* F.S. Melakukan alokasi sebuah elemen dan */
+/* menambahkan elemen pertama dengan nilai val jika alokasi berhasil. */
+/* Jika alokasi gagal: I.S.= F.S. */
+
+void insertLastTodoList(List *l, ElType val);
+/* I.S. l mungkin kosong */
+/* F.S. Melakukan alokasi sebuah elemen dan */
+/* menambahkan elemen list di akhir: elemen terakhir yang baru */
+/* bernilai val jika alokasi berhasil. Jika alokasi gagal: I.S.= F.S. */
+
+void insertAtTodoList(List *l, ElType val, int idx);
+/* I.S. l tidak mungkin kosong, idx indeks yang valid dalam l, yaitu 0..length(l) */
+/* F.S. Melakukan alokasi sebuah elemen dan */
+/* menyisipkan elemen dalam list pada indeks ke-idx (bukan menimpa elemen di i) */
+/* yang bernilai val jika alokasi berhasil. Jika alokasi gagal: I.S.= F.S. */
+
+/*** PENGHAPUSAN ELEMEN ***/
+void deleteFirstTodoList(List *l, ElType *val);
+/* I.S. List l tidak kosong  */
+/* F.S. Elemen pertama list dihapus: nilai info disimpan pada x */
+/*      dan alamat elemen pertama di-dealokasi */
+void deleteLastTodoList(List *l, ElType *val);
+/* I.S. list tidak kosong */
+/* F.S. Elemen terakhir list dihapus: nilai info disimpan pada x */
+/*      dan alamat elemen terakhir di-dealokasi */
+
+void deleteAtTodoList(List *l, int idx, ElType *val);
+/* I.S. list tidak kosong, idx indeks yang valid dalam l, yaitu 0..length(l) */
+/* F.S. val diset dengan elemen l pada indeks ke-idx. */
+/*      Elemen l pada indeks ke-idx dihapus dari l */
+
+/****************** PROSES SEMUA ELEMEN LIST ******************/
+void displayTodoList(List l);
+// void printInfo(List l);
+/* I.S. List mungkin kosong */
+/* F.S. Jika list tidak kosong, iai list dicetak ke kanan: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika list kosong : menulis [] */
+/* Tidak ada tambahan karakter apa pun di awal, akhir, atau di tengah */
+
+int lengthTodoList(List l);
+/* Mengirimkan banyaknya elemen list; mengirimkan 0 jika list kosong */
+
+Item getItemAtLoc(List l, char loc, Tas tas);
+
+#endif
---
+++ src/ADT/originaladt/list_linked.c
@@ -0,0 +1,276 @@
+#include "list_linked.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/* PROTOTYPE */
+/****************** PEMBUATAN LIST KOSONG ******************/
+void CreateList(List *l)
+/* I.S. sembarang             */
+/* F.S. Terbentuk list kosong */
+{
+    FIRST(*l) = NULL;
+}
+
+/****************** TEST LIST KOSONG ******************/
+boolean isEmpty(List l)
+/* Mengirim true jika list kosong */
+{
+    return (FIRST(l) == NULL);
+}
+
+/****************** GETTER SETTER ******************/
+ElType getElmt(List l, int idx)
+/* I.S. l terdefinisi, idx indeks yang valid dalam l, yaitu 0..length(l) */
+/* F.S. Mengembalikan nilai elemen l pada indeks idx */
+{
+    Address p;
+    int counter = 0;
+    p = FIRST(l);
+
+    while (counter < idx)
+    {
+        counter++;
+        p = NEXT(p);
+    }
+    // counter == idx
+    return (INFO(p));
+}
+
+void setElmt(List *l, int idx, ElType val)
+/* I.S. l terdefinisi, idx indeks yang valid dalam l, yaitu 0..length(l) */
+/* F.S. Mengubah elemen l pada indeks ke-idx menjadi val */
+{
+    Address p;
+    int counter = 0;
+    p = FIRST(*l);
+
+    while (counter < idx)
+    {
+        counter++;
+        p = NEXT(p);
+    }
+    // counter == idx
+    INFO(p) = val;
+}
+
+int indexOf(List l, ElType val)
+/* I.S. l, val terdefinisi */
+/* F.S. Mencari apakah ada elemen list l yang bernilai val */
+/* Jika ada, mengembalikan indeks elemen pertama l yang bernilai val */
+/* Mengembalikan IDX_UNDEF jika tidak ditemukan */
+{
+    Address p;
+    p = FIRST(l);
+    int counter = 0;
+    boolean found;
+
+    found = false;
+    while ((NEXT(p) != NULL) && !found)
+    {
+        if (INFO(p) == val)
+        {
+            found = true;
+        }
+        else
+        {
+            counter++;
+            p = NEXT(p);
+        }
+    }
+    if (found)
+    {
+        return (counter);
+    }
+    else
+    {
+        return (IDX_UNDEF);
+    }
+}
+
+/****************** PRIMITIF BERDASARKAN NILAI ******************/
+/*** PENAMBAHAN ELEMEN ***/
+void insertFirst(List *l, ElType val)
+/* I.S. l mungkin kosong */
+/* F.S. Melakukan alokasi sebuah elemen dan */
+/* menambahkan elemen pertama dengan nilai val jika alokasi berhasil. */
+/* Jika alokasi gagal: I.S.= F.S. */
+{
+    Address p;
+
+    p = newNode(val);
+    if (p != NULL)
+    {
+        NEXT(p) = FIRST(*l);
+        FIRST(*l) = p;
+    }
+}
+
+void insertLast(List *l, ElType val)
+/* I.S. l mungkin kosong */
+/* F.S. Melakukan alokasi sebuah elemen dan */
+/* menambahkan elemen list di akhir: elemen terakhir yang baru */
+/* bernilai val jika alokasi berhasil. Jika alokasi gagal: I.S.= F.S. */
+{
+    Address p, new;
+
+    if (isEmpty(*l))
+    {
+        insertFirst(l, val);
+    }
+    else
+    {
+        new = newNode(val);
+        if (new != NULL) // alokasi berhasil
+        {
+            p = FIRST(*l);
+            while (NEXT(p) != NULL)
+            {
+                p = NEXT(p);
+            }
+            // NEXT(p) == NULL {artinya last element)}
+
+            NEXT(p) = new;
+        }
+    }
+}
+
+void insertAt(List *l, ElType val, int idx)
+/* I.S. l tidak mungkin kosong, idx indeks yang valid dalam l, yaitu 0..length(l) */
+/* F.S. Melakukan alokasi sebuah elemen dan */
+/* menyisipkan elemen dalam list pada indeks ke-idx (bukan menimpa elemen di i) */
+/* yang bernilai val jika alokasi berhasil. Jika alokasi gagal: I.S.= F.S. */
+{
+    Address new, p;
+    int counter;
+
+    if (idx == 0)
+    {
+        insertFirst(l, val);
+    }
+    else
+    {
+        new = newNode(val);
+        if (new != NULL)
+        {
+            counter = 0;
+            p = FIRST(*l);
+
+            while (counter < idx - 1)
+            {
+                counter++;
+                p = NEXT(p);
+            }
+            // counter == idx-1
+            NEXT(new) = NEXT(p);
+            NEXT(p) = new;
+        }
+    }
+}
+
+/*** PENGHAPUSAN ELEMEN ***/
+void deleteFirst(List *l, ElType *val)
+/* I.S. List l tidak kosong  */
+/* F.S. Elemen pertama list dihapus: nilai info disimpan pada x */
+/*      dan alamat elemen pertama di-dealokasi */
+{
+    Address p;
+
+    p = FIRST(*l);
+    *val = INFO(p);
+    FIRST(*l) = NEXT(p);
+    free(p);
+}
+
+void deleteLast(List *l, ElType *val)
+/* I.S. list tidak kosong */
+/* F.S. Elemen terakhir list dihapus: nilai info disimpan pada x */
+/*      dan alamat elemen terakhir di-dealokasi */
+{
+    Address p, loc;
+
+    p = FIRST(*l);
+    loc = NULL;
+
+    while (NEXT(p) != NULL)
+    {
+        loc = p;
+        p = NEXT(p);
+    }
+    // NEXT(p) == NULL (artinya last element)
+    if (loc == NULL) // artinya cuman ada 1 elemen di list
+    {
+        FIRST(*l) = NULL;
+    }
+    else
+    {
+        NEXT(loc) = NULL;
+    }
+    *val = INFO(p);
+    free(p);
+}
+
+void deleteAt(List *l, int idx, ElType *val)
+/* I.S. list tidak kosong, idx indeks yang valid dalam l, yaitu 0..length(l) */
+/* F.S. val diset dengan elemen l pada indeks ke-idx. */
+/*      Elemen l pada indeks ke-idx dihapus dari l */
+{
+    Address p, loc;
+    int counter;
+
+    if (idx == 0)
+    {
+        deleteFirst(l, val);
+    }
+    else
+    {
+        counter = 0;
+        loc = FIRST(*l);
+
+        while (counter < idx - 1)
+        {
+            counter++;
+            loc = NEXT(loc);
+        }
+        p = NEXT(loc);
+        *val = INFO(p);
+        NEXT(loc) = NEXT(p);
+        NEXT(p) = NULL;
+        free(p);
+    }
+}
+
+/****************** PROSES SEMUA ELEMEN LIST ******************/
+void displayList(List l)
+// void printInfo(List l);
+/* I.S. List mungkin kosong */
+/* F.S. Jika list tidak kosong, iai list dicetak ke kanan: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika list kosong : menulis [] */
+/* Tidak ada tambahan karakter apa pun di awal, akhir, atau di tengah */
+{
+    Address p;
+
+    p = FIRST(l);
+    printf("[");
+    while (NEXT(p) != NULL)
+    {
+        printf("%d,", INFO(p));
+        p = NEXT(p);
+    }
+    printf("%d]", INFO(p));
+}
+
+int length(List l)
+/* Mengirimkan banyaknya elemen list; mengirimkan 0 jika list kosong */
+{
+    Address p;
+    int count = 0;
+    p = FIRST(l);
+
+    while (p != NULL)
+    {
+        count++;
+        p = NEXT(p);
+    }
+    return count;
+}
